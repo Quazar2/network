ec3d0b00b7f862fb2ba5650552c6f00205234011
Quazar2 <pierrebarbier741@gmail.com>
2024-10-17 23:51:40 +0200

    So simple it's not worth doing, project abandonned !

--- network.h
+++ network.h
@@ -1,10 +1,10 @@
 #pragma once
+#include <stdlib.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <netdb.h>
+#include <string.h>
+#include <stdio.h>
 
-typedef struct c_Server{
-	int Socket_FD;
-	int* Connect_FD;
-}c_Server_t;
+//Actually seen that boilerplate is soshort i might as well write it everytime.
